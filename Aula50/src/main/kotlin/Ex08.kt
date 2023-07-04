0f34717df02469a4c6ad7159f86e192ab2842614
paula.nascimento <paula.nascimento@zup.com.br>
2023-07-04 10:24:56 -0300

    corrigindo exercício 8

--- Aula50/src/main/kotlin/Ex08.kt
+++ Aula50/src/main/kotlin/Ex08.kt
@@ -4,6 +4,27 @@ class Ex08 {
             print("Digite um número: ")
             val x = readln().toInt()
 
+            var n1 = 0
+            var n2 = 1
+            var fib = 0
+
+            if(x == 0){
+                println("Fibonacci($x) = $n1")
+            } else if(x == 1){
+                println("Fibonacci($x) = $n2")
+            } else if (x < 0){
+                println("Número Inválido")
+            } else {
+                var i = 2
+                while(i <= x){
+                    fib = n1 + n2
+                    val aux = n1
+                    n1 = n2
+                    n2 = aux + n1
+                    i++
+                }
+                println("Fibonacci($x) = $fib")
+            }
         }
     }
 }
